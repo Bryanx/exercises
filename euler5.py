511629e3f7c53af870ad4547e05e550b1a5d39c6
Bryanx <br.deridder@gmail.com>
2016-04-07 00:17:10 +0200

    Update euler5.py

--- euler5.py
+++ euler5.py
@@ -1,15 +1,14 @@
 def find_smallest_multiple(a, b):
-    e, x = [], 2400
-    for c in range(x, 3000, 10):
+    e, x = [], 10
+    for c in range(x, 240000000, 10):
         for d in range(a, b+1):
             if c % d == 0:
                 if a == b:
                     return c
                 else:
-                    print (c, d, a)
                     a += 1
             else:
                 a = 3
                 x += 10
 
-print(find_smallest_multiple(3, 10))
+print(find_smallest_multiple(2, 20))
