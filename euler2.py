fe5380395b25b736e23f986b19d3b198ad2e5e83
Bryanx <br.deridder@gmail.com>
2016-04-06 18:37:26 +0200

    Update euler2.py

--- euler2.py
+++ euler2.py
@@ -1,11 +1,17 @@
 def even_fibonacci_sum(x):
     f = [1, 2]
     e = []
+    
+    # keep generating fibonacci number untill x is reached:
     while f[len(f) - 2] + f[len(f) - 1] < x:
         f.append(f[len(f) - 2] + f[len(f) - 1])
+        
+    # remove all uneven fibonacci numbers:
     for i in f:
         if i % 2 == 0:
             e.append(i)
+            
+    # return the sum of all even numbers:
     return sum(e)
 
 
