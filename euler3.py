7f5d0c784c7d76d33d8e5eb71b235f553f70d791
Bryanx <br.deridder@gmail.com>
2016-04-06 18:34:46 +0200

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -1,8 +1,10 @@
 import math
 
+# find all uneven divisors of sqrt x:
 def divisor(x):
     return [i for i in range(3, int(math.sqrt(x)), 2) if x % i == 0]
 
+#find all uneven divisors of sqrt x that are prime:
 def prime_factors(x):
     e = []
     for a in divisor(x):
