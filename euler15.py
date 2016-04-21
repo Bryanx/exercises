69231b3cddda2c46688b0427b93372608167d0a5
Bryan de Ridder <br.deridder@gmail.com>
2016-04-21 11:28:38 +0200

    Create euler15.py

---
+++ euler15.py
@@ -0,0 +1,34 @@
+import time
+
+start_time = time.time()
+begin = [0,0]
+after = [2,2]
+count = 0
+uniq_seq = []
+
+while sum(begin) != 4:
+    if begin[0] == 0 and :
+        begin[0] += 1
+    if begin[1] == 0:
+        begin[1] += 1
+
+
+
+
+
+# lst = []
+# right = 0
+# down = 0
+# test = []
+#
+# while lst != [2,2]:
+#     if right + 1 != 2:
+#         right += 1
+#     elif down + 1 !=2:
+#         down += 1
+#     test.append(down+right)
+
+
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
