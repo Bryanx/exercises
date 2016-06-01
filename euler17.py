267caccc8cfb5c145f1b74085c5c8d428b347f6a
Bryan de Ridder <br.deridder@gmail.com>
2016-06-01 15:18:17 +0200

    Create euler17.py

---
+++ euler17.py
@@ -0,0 +1,15 @@
+import time;
+
+start_time = time.time()
+
+one_nine = 36
+tent_nineteen = 71
+twenty_ninety = 47
+hundredand = 7 + 3
+thousand = 8
+
+print(one_nine + hundredand + (one_nine * 8 + tent_nineteen + twenty_ninety))
+
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
