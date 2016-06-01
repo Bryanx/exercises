8c7724ec5d12fdd15aff9fbf83992845d21d0194
Bryan de Ridder <br.deridder@gmail.com>
2016-06-02 00:40:54 +0200

    Update euler17.py

--- euler17.py
+++ euler17.py
@@ -1,15 +1,8 @@
-import time;
+one_to_nine = 36
+ten_to_nineteen = 70
+twenty_ninety = 46
+hundredand = 10
+onethousand = 11
+hundred = 7
 
-start_time = time.time()
-
-one_nine = 36
-tent_nineteen = 71
-twenty_ninety = 47
-hundredand = 7 + 3
-thousand = 8
-
-print(one_nine + hundredand + (one_nine * 8 + tent_nineteen + twenty_ninety))
-
-
-
-print("--- %s seconds ---" % (time.time() - start_time))
+print((one_to_nine+hundred*9)+(one_to_nine+hundredand*9)*99+(one_to_nine*9+ten_to_nineteen+twenty_ninety*10)*10+(onethousand))
