a454008e7c263f9d2286e1cc16e517b5038f8b41
Bryanx <br.deridder@gmail.com>
2016-09-28 23:22:59 +0200

    Opdrachten P1W2

---
+++ Java/1 Basisbegrippen/P1W2 Opdracht Belasting/src/belasting/Belasting.java
@@ -0,0 +1,8 @@
+package belasting;
+
+/**
+ * @author Bryan de Ridder
+ * @version 1.0 28/09/16 10:04
+ */
+public class Belasting {
+}
---
+++ Java/1 Basisbegrippen/P1W2 Opdracht Cijfers/src/cijfers/Cijfers.java
@@ -0,0 +1,8 @@
+package cijfers;
+
+/**
+ * @author Bryan de Ridder
+ * @version 1.0 28/09/16 15:53
+ */
+public class Cijfers {
+}
