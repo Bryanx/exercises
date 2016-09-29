4125f749fb945fef68f4dfaef485b088fc0e8b63
Bryanx <br.deridder@gmail.com>
2016-09-29 20:11:08 +0200

    Changed code with new toCharArray

--- Java/1 Basisbegrippen/P1W2 Opdracht Codering/src/codering/Codering.java
+++ Java/1 Basisbegrippen/P1W2 Opdracht Codering/src/codering/Codering.java
@@ -10,13 +10,36 @@ public class Codering {
     public static void main(String[] args) {
         Scanner keyboardScanner = new Scanner(System.in);
 
-        String bericht;
-        int verschuiven;
+        final int ASCII_SPATIE = 32;
+        final int ASCII_Z_CAPS = 90;
+        final int ASCII_B_CAPS = 26;
+        String gedecodeerdBericht;
+        String gecodeerdBericht = "";
+        int verschuiving;
+        int ascii;
 
-        System.out.println("Geef een bericht: ");
+        System.out.print("Geef een bericht: ");
+        gedecodeerdBericht = keyboardScanner.nextLine();
+        System.out.print("Hoeveel letters verschuiven: ");
+        verschuiving = keyboardScanner.nextInt();
 
-        System.out.println("Hoeveel letters verschuiven: ");
-
-
+        //iedere letter in de string wordt omgezet naar ascii
+        //daarna wordt asciiwaarde verschoven nav het aantal verschuivingen.
+        //daarna naar terug naar een string.
+        for (char karakter : gedecodeerdBericht.toCharArray()) {
+            if ((int) karakter == ASCII_SPATIE) {
+                gecodeerdBericht += " ";
+            } else if ((int) karakter > ASCII_Z_CAPS - verschuiving) {
+                ascii = (int) karakter - ASCII_B_CAPS + verschuiving;
+                gecodeerdBericht += (char) ascii;
+            } else {
+                ascii = (int) karakter + verschuiving;
+                gecodeerdBericht += (char) ascii;
+            }
+        }
+        System.out.print("Gecodeerd bericht:");
+        System.out.println(gecodeerdBericht);
+        System.out.print("Gedecodeerd bericht:");
+        System.out.println(gedecodeerdBericht);
     }
 }
