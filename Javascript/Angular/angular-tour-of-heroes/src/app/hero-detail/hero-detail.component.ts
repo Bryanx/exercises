182a5caafcaea45e9c024909c63d080ef72d5054
Bryan <br.deridder@gmail.com>
2018-06-27 10:47:27 +0200

    update routes

--- Javascript/Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ Javascript/Angular/angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,18 +1,33 @@
-
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../models/hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Hero }         from '../models/hero';
+import { HeroService }  from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
- 
-  constructor() { }
- 
-  ngOnInit() {
+
+  constructor(
+    private route: ActivatedRoute, //de huidige url + id
+    private location: Location, //hiermee kunnen we interacteren met de browser (back button)
+    private heroService: HeroService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
   }
- 
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id'); //haal de id uit de url en zet om naar een int (met +)
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
